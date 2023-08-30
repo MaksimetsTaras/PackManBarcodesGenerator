7c467bfc4523d73e32428b756ae51bac94b45dd6
Taras Maksimets <taras.maksimets@gmail.com>
2023-08-30 10:11:45 +0300

    Added TopAppBar

--- app/src/main/java/com/example/packmanbarcodesgenerator/MainScreen.kt
+++ app/src/main/java/com/example/packmanbarcodesgenerator/MainScreen.kt
@@ -1,24 +1,22 @@
 package com.example.packmanbarcodesgenerator
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Colors
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,14 +24,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -42,6 +39,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.packmanbarcodesgenerator.screens.BoxScreen
 import com.example.packmanbarcodesgenerator.screens.PartScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
@@ -50,13 +48,44 @@ fun MainScreen() {
     val backStackEntry = navController.currentBackStackEntryAsState()
 
     var bottomNavHeight by remember { mutableStateOf(0.dp) }
+    var topAppHeight by remember { mutableStateOf(0.dp) }
     // get local density from composable
     val density = LocalDensity.current
+
+    val context = LocalContext.current
 
     Scaffold(
         modifier = Modifier
             .fillMaxSize(),
         containerColor = Color.Transparent,
+        topBar = {
+            TopAppBar(
+                modifier = Modifier
+                    .onGloballyPositioned {
+                        topAppHeight = with(density) {
+                            it.size.height.toDp()
+                        }
+                    }
+                    .clip(RoundedCornerShape(0.dp, 0.dp, 30.dp, 30.dp)),
+                colors = topAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_200),
+                    titleContentColor = colorResource(id = R.color.black),
+                ),
+                title = {
+                    Text(stringResource(R.string.app_name))
+                },
+                actions = {
+                    val iconSave = painterResource(id = R.drawable.save)
+
+                    IconButton(onClick = { makeToast(context, "Save") }) {
+                        Icon(painter = iconSave, contentDescription = null)
+                    }
+//                    IconButton(onClick = { makeToast(context, "Save") }) {
+//                        Icon(painter = icon2, contentDescription = null)
+//                    }
+                },
+            )
+        },
         bottomBar = {
             val bottomNavItems = arrayListOf<bottomNavItem>()
             bottomNavItems.add(bottomNavItem("Box", "Box", R.drawable.box))
@@ -66,7 +95,7 @@ fun MainScreen() {
                 backgroundColor = colorResource(id = R.color.purple_200),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clip(RoundedCornerShape(15.dp, 15.dp, 0.dp, 0.dp))
+                    .clip(RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp))
                     .onGloballyPositioned {
                         bottomNavHeight = with(density) {
                             it.size.height.toDp()
@@ -99,7 +128,7 @@ fun MainScreen() {
             startDestination = "Box",
             modifier = Modifier
                 .fillMaxSize()
-                .padding(bottom = bottomNavHeight)
+                .padding(bottom = bottomNavHeight, top = topAppHeight)
         ) {
             composable("Box") {
                 BoxScreen()
@@ -110,4 +139,8 @@ fun MainScreen() {
             }
         }
     }
+}
+
+fun makeToast(ctx: Context, msg: String) {
+    Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show()
 }
